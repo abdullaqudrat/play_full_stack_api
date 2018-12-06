1d7043d3cd11e478e94bca8a0af03206c1094087
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-06 12:56:05 -0700

    More progress on seeding playlists favorites

--- db/seeds/dev/playlists_favorites.js
+++ db/seeds/dev/playlists_favorites.js
@@ -1,9 +1,17 @@
+pry = require('pryjs')
+// eval(pry.it)
+
+
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
+
   return knex('playlists_favorites').del()
-    .then(knex('playlists').del())
-    .then(knex('favorites').del())
+    .then(() => knex('playlists').del())
+    .then(() => knex('favorites').del())
+    .then(console.log('Deleted all'))
+    .catch(error => console.log(`Error deleting: ${ error }`))
+
     .then(function () {
 
       const favorites =
@@ -24,16 +32,55 @@ exports.seed = function(knex, Promise) {
 
       return(Promise.all([
 
+        // favorites.forEach(function(favorite) {
+        //   makeFavorite(knex, favorite)
+        // }),
         knex('favorites').insert([
           { song_title: 'Roxanne', artist_name: 'The Police', genre: 'Rock', song_rating: '34' },
           { song_title: 'Cloudy', artist_name: 'Funkmammoth', genre: 'Funk', song_rating: '23' },
           { song_title: 'No Quarter', artist_name: 'Led Zeppelin', genre: 'Rock', song_rating: '45'}
         ]),
-        knex('playlists').insert(
-          [{ name: 'sleep now'}, { name: 'disco'}]
-        )
-
+        knex('playlists').insert([
+          { name: 'sleep now'}, { name: 'disco'}
+        ])
       ]))
       // Inserts seed entries
     })
+    .then(function () {
+      return knex('favorites').pluck('id').then(function(favoriteIds) {
+        console.log(favoriteIds)
+        var faveIds = favoriteIds
+        console.log(faveIds)
+        return knex('playlists').pluck('id').then(function(playlistIds) {
+          var playIds = playlistIds
+          console.log(faveIds)
+          console.log(playIds)
+
+          var fave_play_ids = faveIds.map(function(faveId, index) {
+            return [faveId, playIds[Math.floor(Math.random()*playIds.length)] ]
+          });
+          console.log(fave_play_ids)
+          var favyId = fave_play_ids.forEach(function(fave_play_id) {
+            console.log(fave_play_id[0])
+            console.log(fave_play_id[1])
+            return knex('playlists_favorites').insert({ favorite_id: fave_play_id[0], playlist_id: fave_play_id[1] }, 'id');
+          })
+          console.log(favyId)
+
+        })
+
+        // eval(pry.it)
+        console.log(fave_play_ids)
+        // faveIds.forEach(function(favorite_id) {
+        //
+        //   return knex('playlists_favorites').insert({ favorite_id: favorite_id });
+        // })
+      })
+    })
+    .then(function() {
+      return knex('playlists').pluck('id').then(function(ids) {
+        console.log(ids)
+      })
+    })
+
 }
