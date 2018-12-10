60198470b63a8b2abe3ea669a7aa876d109adfea
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-10 11:21:25 -0700

    Abstract favorites index route to routes file

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const welcomeController = require('../app/controllers/welcome-controller')
-
 const welcomeRoute = router.get('/', welcomeController.index);
+
+const favoritesController = require('../app/controllers/api/v1/favorites-controller')
+const favoritesRoute = router.get('/api/v1/favorites', favoritesController.index)
 
 module.exports = {
   welcomeRoute,
+  favoritesIndexRoute,
 }
