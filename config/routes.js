0b8356fddb0f779a9a6622070936dc51efaa92a3
abdullaqudrat <aqudrat630@gmail,com>
2018-12-10 11:52:03 -0700

    Adds playlists index route

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,11 @@ const welcomeRoute = router.get('/', welcomeController.index);
 const favoritesController = require('../app/controllers/api/v1/favorites-controller')
 const favoritesIndexRoute = router.get('/api/v1/favorites', favoritesController.index)
 
+const playlistsController = require('../app/controllers/api/v1/playlists-controller')
+const playlistsIndexRoute = router.get('/api/v1/playlists', playlistsController.index)
+
 module.exports = {
   welcomeRoute,
   favoritesIndexRoute,
+  playlistsIndexRoute,
 }
