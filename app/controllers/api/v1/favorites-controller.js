835139b7017b6ab95beb87e624e4c3926f3c232c
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 14:17:31 -0700

    Adds favorite create action

--- app/controllers/api/v1/favorites-controller.js
+++ app/controllers/api/v1/favorites-controller.js
@@ -31,7 +31,27 @@ const show = (request, response) => {
     });
 }
 
+const create = (request, response) => {
+  const song = request.body;
+
+  for (let requiredParameter of ['song_title', 'artist_name', 'genre', 'song_rating']) {
+    if (!song[requiredParameter]) {
+      return response
+      .status(422)
+      .send({ error: `Expected format: { song_title: <String>, artist_name: <String>, genre: <String>, song_rating: <Integer> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+  Favorite.create(song, ['id', 'song_title', 'artist_name', 'song_rating'])
+    .then(song => {
+      response.status(201).json({ id: song[0], song_title: song[1], artist_name: song[2], genre: song[3], song_rating: song[4]})
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+  }
+
 module.exports = {
   index,
   show,
+  create,
 }
