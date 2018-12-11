91e5f52bcf2ba362ea5f84b9e49a3fe5861e06f5
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 14:19:54 -0700

    Cleans up unused/disorganized code

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const app = express();
 const route = require('./config/routes')
-
-app.use('/', route.welcomeRoute);
-app.use('/api/v1/favorites', route.favoritesIndexRoute);
-
-// app.use('/api/v1/playlists', route.playlistsIndexRoute);
-// app.use('/api/v1/playlists/:id/songs', route.playlistsIndexRoute);
-
-
 const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
@@ -20,38 +12,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Play API';
 
-
-
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
+app.use('/', route.welcomeRoute);
+app.use('/api/v1/favorites', route.favoritesIndexRoute);
+app.use('/api/v1/favorite/:id', route.favoritesShowRoute);
+app.use('/api/v1/favorites', route.favoritesCreateRoute);
 
-
-
-
-// POST NEW FAVORITE
-
-app.post('/api/v1/favorites', (request, response) => {
-  const song = request.body;
-
-  for (let requiredParameter of ['song_title', 'artist_name', 'genre', 'song_rating']) {
-    if (!song[requiredParameter]) {
-      return response
-        .status(422)
-        .send({ error: `Expected format: { song_title: <String>, artist_name: <String>, genre: <String>, song_rating: <Integer> }. You're missing a "${requiredParameter}" property.` });
-    }
-  }
-
-  database('favorites').insert(song, ['id', 'song_title', 'artist_name', 'song_rating'])
-    .then(song => {
-      response.status(201).json({ id: song[0], song_title: song[1], artist_name: song[2], genre: song[3], song_rating: song[4]})
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
+app.use('/api/v1/playlists', route.playlistsIndexRoute);
+app.use('/api/v1/playlists/:playlist_id/favorites', route.playlistsShowRoute);
+app.use('/api/v1/playlists/:playlist_id/favorites/:id', route.playlistsCreateRoute);
+app.use('/api/v1/playlists/:playlist_id/favorites/:id', route.playlistsDestroyRoute);
 
 // PATCH FAVORITE
 
