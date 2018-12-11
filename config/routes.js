38c76ba22922b62db271f1974b2aaad7aa324c34
abdullaqudrat <aqudrat630@gmail,com>
2018-12-10 20:55:55 -0700

    Adds playlist delete route

--- config/routes.js
+++ config/routes.js
@@ -10,10 +10,12 @@ const favoritesIndexRoute = router.get('/api/v1/favorites', favoritesController.
 const playlistsController = require('../app/controllers/api/v1/playlists-controller')
 // const playlistsIndexRoute = router.get('/api/v1/playlists', playlistsController.index)
 const playlistsShowRoute = router.get('/api/v1/playlists/:id/songs', playlistsController.show)
+const playlistsDestroyRoute = router.get('/api/v1/playlists/:id/songs', playlistsController.show)
 
 module.exports = {
   welcomeRoute,
   favoritesIndexRoute,
   // playlistsIndexRoute,
-  playlistsShowRoute
+  playlistsShowRoute,
+  playlistsDestroyRoute,
 }
