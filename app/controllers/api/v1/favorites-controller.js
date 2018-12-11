1b6e728dc24e9290bf983fb0ee6daf64ef2e9fe7
abdullaqudrat <aqudrat630@gmail,com>
2018-12-10 22:36:08 -0700

    Adds favorite show action

--- app/controllers/api/v1/favorites-controller.js
+++ app/controllers/api/v1/favorites-controller.js
@@ -15,6 +15,23 @@ const index = (request, response) => {
     })
 }
 
+const show = (request, response) => {
+  Favorite.find(request.params.id)
+    .then(favorites => {
+      if (favorites.length) {
+        response.status(200).json({ id: favorites[0], song_title: favorites[1], artist_name: favorites[2], genre: favorites[3], song_rating: favorites[4]});
+      } else {
+        response.status(404).json({
+          error: `Could not find song with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+}
+
 module.exports = {
   index,
+  show,
 }
