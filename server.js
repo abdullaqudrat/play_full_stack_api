ad649f9c3076247c28d9ef1a295144996ac9feb2
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 14:57:58 -0700

    Removes unused code

--- server.js
+++ server.js
@@ -20,33 +20,13 @@ app.use('/', route.welcomeRoute);
 app.use('/api/v1/favorites', route.favoritesIndexRoute);
 app.use('/api/v1/favorite/:id', route.favoritesShowRoute);
 app.use('/api/v1/favorites', route.favoritesCreateRoute);
+app.use('/api/v1/favorites/:id', route.favoritesUpdateRoute);
 
 app.use('/api/v1/playlists', route.playlistsIndexRoute);
 app.use('/api/v1/playlists/:playlist_id/favorites', route.playlistsShowRoute);
 app.use('/api/v1/playlists/:playlist_id/favorites/:id', route.playlistsCreateRoute);
 app.use('/api/v1/playlists/:playlist_id/favorites/:id', route.playlistsDestroyRoute);
 
-// PATCH FAVORITE
-
-app.patch('/api/v1/favorites/:id', (request, response) => {
-  const song = request.body;
-
-  for (let requiredParameter of ['song_title', 'artist_name', 'genre', 'song_rating']) {
-    if (!song[requiredParameter]) {
-      return response
-        .status(422)
-        .send({ error: `Expected format: { song_title: <String>, artist_name: <String>, genre: <String>, song_rating: <Integer> }. You're missing a "${requiredParameter}" property.` });
-    }
-  }
-
-  database('favorites').where({ id: request.params.id }).update(song, ['id', 'song_title', 'artist_name', 'song_rating'])
-    .then(song => {
-      response.status(200).json({ id: song[0], song_title: song[1], artist_name: song[2], genre: song[3], song_rating: song[4]})
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
 
 // DELETE FAVORITES
 
