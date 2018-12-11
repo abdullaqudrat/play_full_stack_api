eb5f46ad93c291da0f7483d7e192211d6fc4561d
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 15:42:27 -0700

    Removes unused code/Adds destroy favorite use function
    deletes unused code
    --destroy favorite ep now in favorite controller
    --database setup now in favorite and playlist models

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express();
 const route = require('./config/routes')
 const bodyParser = require('body-parser');
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
@@ -21,22 +17,9 @@ app.use('/api/v1/favorites', route.favoritesIndexRoute);
 app.use('/api/v1/favorite/:id', route.favoritesShowRoute);
 app.use('/api/v1/favorites', route.favoritesCreateRoute);
 app.use('/api/v1/favorites/:id', route.favoritesUpdateRoute);
+app.use('/api/v1/favorites/:id', route.favoritesDestroyRoute);
 
 app.use('/api/v1/playlists', route.playlistsIndexRoute);
 app.use('/api/v1/playlists/:playlist_id/favorites', route.playlistsShowRoute);
 app.use('/api/v1/playlists/:playlist_id/favorites/:id', route.playlistsCreateRoute);
 app.use('/api/v1/playlists/:playlist_id/favorites/:id', route.playlistsDestroyRoute);
-
-
-// DELETE FAVORITES
-
-app.delete('/api/v1/favorites/:id', (request, response) => {
-
-  database('favorites').where({ id: request.params.id }).del()
-    .then(song => {
-      response.status(204).json()
-    })
-    .catch(error => {
-      response.status(404).json({ error });
-    });
-});
