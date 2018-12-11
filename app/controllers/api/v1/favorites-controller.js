5980708a7b77c55e87db0c16bb61d44cf1973b93
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 14:57:11 -0700

    Adds update action

--- app/controllers/api/v1/favorites-controller.js
+++ app/controllers/api/v1/favorites-controller.js
@@ -50,8 +50,29 @@ const create = (request, response) => {
     });
   }
 
+  const update = (request, response) => {
+    const song = request.body;
+
+  for (let requiredParameter of ['song_title', 'artist_name', 'genre', 'song_rating']) {
+    if (!song[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { song_title: <String>, artist_name: <String>, genre: <String>, song_rating: <Integer> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  Favorite.update(request.params.id, song, ['id', 'song_title', 'artist_name', 'song_rating'])
+    .then(song => {
+      response.status(200).json({ id: song[0], song_title: song[1], artist_name: song[2], genre: song[3], song_rating: song[4]})
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+  }
+
 module.exports = {
   index,
   show,
   create,
+  update,
 }
