2d2d3c2c93b914cb6698c4620b8bb30d766e383b
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 22:58:44 -0700

    Updates playlist show page to fit spec

--- app/controllers/api/v1/playlists-controller.js
+++ app/controllers/api/v1/playlists-controller.js
@@ -60,23 +60,23 @@ const show = (request, response) => {
     Playlist.find(request.params.id)
     .then(playlist => {
     if (playlist.length) {
-      var favorites = playlist.map(function(p, index) {
-        return { song_title: p["song_title"],
-                 artist_name: p["artist_name"],
-                 genre: p["genre"],
-                 song_rating: p["song_rating"]
+      var favorites = playlist.map(function(favorite, index) {
+        return { 
+                id: favorite["id"],
+                song_title: favorite["song_title"],
+                artist_name: favorite["artist_name"],
+                genre: favorite["genre"],
+                song_rating: favorite["song_rating"]
                }
       })
 
       response.status(200).json({
         id: playlist[0]["playlist_id"],
-        playlist_name: playlist[0]["name"],
+        name: playlist[0]["name"],
         favorites
       })
     } else {
-      response.status(404).json({
-        error: `Could not find playlist with id ${request.params.id}`
-      });
+      response.status(404).json({ "error": `Could not find playlist with id ${request.params.id}`});
     }
   })
   .catch(error => {
