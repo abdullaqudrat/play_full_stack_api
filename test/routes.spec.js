2c591369f4fc3ed2a879cfc641e2c942ce6653ae
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 18:48:59 -0700

    Changes spec file
    --configures testing environment
    --adds build up and tear down of DB
    --changes expectations for favorites request test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,9 +1,38 @@
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
+
+beforeEach((done) => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  afterEach((done) => {
+    database.migrate.rollback()
+    .then(() => done())
+    .catch(error => {
+        throw error;
+      });
+  });
 
 describe('API Routes', () => {
     it('should return the homepage with text', done => {
@@ -28,46 +57,38 @@ describe('API Routes', () => {
         chai.request(server)
         .get('/api/v1/favorites')
         .end((err, response) => {
-          response.should.have.status(200);
-          response.should.be.json;
-          response.res.text.should.equal('[{"id":1,"song_title":"hello","artist_name":"world","genre":"rock","song_rating":1,"created_at":"2018-12-04T22:41:24.675Z","updated_at":"2018-12-04T22:41:24.675Z"},{"id":2,"song_title":"hello","artist_name":"world","genre":"rock","song_rating":1,"created_at":"2018-12-04T22:42:02.576Z","updated_at":"2018-12-04T22:42:02.576Z"},{"id":3,"song_title":"hello","artist_name":"world","genre":"rock","song_rating":1,"created_at":"2018-12-04T22:42:23.914Z","updated_at":"2018-12-04T22:42:23.914Z"},{"id":4,"song_title":"hello","artist_name":"world","genre":"rock","song_rating":1,"created_at":"2018-12-04T23:45:44.661Z","updated_at":"2018-12-04T23:45:44.661Z"},{"id":5,"song_title":"hello","artist_name":"world","genre":"rock","song_rating":1,"created_at":"2018-12-04T23:46:26.423Z","updated_at":"2018-12-04T23:46:26.423Z"},{"id":6,"song_title":"hello","artist_name":"world","genre":"rock","song_rating":1,"created_at":"2018-12-04T23:46:34.889Z","updated_at":"2018-12-04T23:46:34.889Z"},{"id":7,"song_title":"hello","artist_name":"world","genre":"rock","song_rating":1,"created_at":"2018-12-04T23:47:43.058Z","updated_at":"2018-12-04T23:47:43.058Z"}]');
-          done();
-        });
-    });
-});
-
-describe('My API routes', () => {
-    before((done) => {
-        database.migrate.latest()
-        .then( () => done())
-        .catch(error => {
-            throw error;
-        });
-    });
-    beforeEach((done) => {
-        database.seed.run()
-        .then( () => done())
-        .catch(error => {
-            throw error;
-        });
-    });
-    // after((done) => {
-
-    // });
-
-    it('should create a thing', done => {
-        chai.request(server)
-        .post('/api/v1/something')
-        .send({
-            thing1: 'this',
-            thing2: 'that'
-        });
-        .end((err, response) => {
-            response.should.have.status(201);
-            response.should.be.a('object');
-            response.should.have.property('id');
+            response.should.have.status(200);
+            response.should.be.json;
+            response.body.should.be.a('array');
+            response.body.length.should.equal(3);
+            response.body[0].should.have.property('id');
+            response.body[0].id.should.equal(1);
+            response.body[0].should.have.property('song_title');
+            response.body[0].song_title.should.equal('Roxanne');
+            response.body[0].should.have.property('artist_name');
+            response.body[0].artist_name.should.equal('The Police');
+            response.body[0].should.have.property('genre');
+            response.body[0].genre.should.equal('Rock');
+            response.body[0].should.have.property('song_rating');
+            response.body[0].song_rating.should.equal(34);
             done();
         });
     });
 });
+
+//     it('should create a thing', done => {
+//         chai.request(server)
+//         .post('/api/v1/something')
+//         .send({
+//             thing1: 'this',
+//             thing2: 'that'
+//         });
+//         .end((err, response) => {
+//             response.should.have.status(201);
+//             response.should.be.a('object');
+//             response.should.have.property('id');
+//             done();
+//         });
+//     });
+// });
 
