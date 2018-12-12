53fd5b266d5ea3b445bf228f641d2f73cdc42b11
abdullaqudrat <aqudrat630@gmail,com>
2018-12-11 22:34:10 -0700

    adds test for playlist index endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -211,6 +211,22 @@ describe('API Routes', () => {
           done();
         });
     });
+    it('should return playlists', done => {
+        chai.request(server)
+        .get('/api/v1/playlists')
+        .end((err, response) => {
+            response.should.have.status(200);
+            response.should.be.json;
+            response.body.should.be.a('array');
+            response.body[0].should.have.property('id');
+            response.body[0].should.have.property('name');
+            response.body[0]['favorites'][0].should.have.property('song_title');
+            response.body[0]['favorites'][0].should.have.property('artist_name');
+            response.body[0]['favorites'][0].should.have.property('genre');
+            response.body[0]['favorites'][0].should.have.property('song_rating');
+            done();
+        });
+    });
 });
 
 
